b5af1390f0291b18ab2350a539b75478ade7f749
yueli <yueli@linkcm.com>
2020-09-14 20:34:31 +0800

    1.application.properties配置属性

--- user-service-provider/src/main/java/com/yueli/demo/Application.java
+++ user-service-provider/src/main/java/com/yueli/demo/Application.java
@@ -1,13 +1,30 @@
 package com.yueli.demo;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.ServiceConfig;
+import com.yueli.demo.service.impl.GreetingsServiceImpl;
+import com.yueli.service.GreetingsService;
+
 public class Application {
+    private static String zookeeperHost = System.getProperty("zookeeper.address", "127.0.0.1");
 
     public static void main(String[] args) throws Exception {
+    	/*ServiceConfig<GreetingsService> service = new ServiceConfig<>();
+        service.setApplication(new ApplicationConfig("first-dubbo-provider"));
+        service.setRegistry(new RegistryConfig("zookeeper://" + zookeeperHost + ":2181"));
+        service.setInterface(GreetingsService.class);
+        service.setRef(new GreetingsServiceImpl());
+        service.export();*/
+    	
     	 ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"provider.xml"});
     	 context.start();
-    	 System.out.println("dubbo service started");
-         System.in.read(); 
+
+        System.out.println("dubbo service started");
+        new CountDownLatch(1).await();
     }
 }
--- user-service.consumer/src/main/java/com/yueli/consumer/Application.java
+++ user-service.consumer/src/main/java/com/yueli/consumer/Application.java
@@ -4,21 +4,34 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 import com.yueli.consumer.service.impl.PersonServiceImpl;
+import com.yueli.service.GreetingsService;
 
 @EnableDubboConfig
 public class Application {	
 	
 	private static Logger log=Logger.getLogger(Application.class);
+    private static String zookeeperHost = System.getProperty("zookeeper.address", "127.0.0.1");
 
     @SuppressWarnings("resource")
-    public static void main(String[] args) {
-		  ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"consumer.xml"});
-          context.start();
-        try {
-          PersonServiceImpl service =context.getBean("personServiceImpl", PersonServiceImpl.class);;
-          String message = service.attributes();
-          System.out.println(message);
+    public static void main(String[] args) throws Exception {
+    	/*ReferenceConfig<GreetingsService> reference = new ReferenceConfig<>();
+        reference.setApplication(new ApplicationConfig("first-dubbo-consumer"));
+        reference.setRegistry(new RegistryConfig("zookeeper://" + zookeeperHost + ":2181"));
+        reference.setInterface(GreetingsService.class);
+        GreetingsService service = reference.get();
+        String message = service.sayHi("dubbo");
+        System.out.println(message);*/
+    	
+    	 ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"consumer.xml"});
+         context.start();
+       try {
+         PersonServiceImpl service =context.getBean("personServiceImpl", PersonServiceImpl.class);;
+         String message = service.attributes();
+         System.out.println(message);
 		  System.in.read();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
